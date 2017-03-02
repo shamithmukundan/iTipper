9d5af23f09b8abfeef3ca6bf73ba568855b7ad76
shamithmukundan <shamith.m@gmail.com>
2017-03-01 22:49:38 -0800

    Initial Commit

---
+++ iTipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTipper
+//
+//  Created by Shamith Mukundan on 3/1/17.
+//  Copyright © 2017 Shamith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
